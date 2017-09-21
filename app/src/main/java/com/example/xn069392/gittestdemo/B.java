5721b8861ee5cf1b2a7cb3c235fa29a81191138c
zhouhao9@xiaoniu66.com <zhouhao9@xiaoniu66.com>
2017-09-21 11:48:10 +0800

    -m首次提交

---
+++ app/src/main/java/com/example/xn069392/gittestdemo/B.java
@@ -0,0 +1,15 @@
+package com.example.xn069392.gittestdemo;
+
+import android.util.Log;
+
+/**
+ * Created by xn069392 on 2017/9/21.
+ */
+
+public class B {
+    private static final String TAG = "bbbb";
+
+    public static  void  gogogo(){
+        Log.e(TAG, "gogogo: ");
+    }
+}
---
+++ app/src/main/java/com/example/xn069392/gittestdemo/C.java
@@ -0,0 +1,15 @@
+package com.example.xn069392.gittestdemo;
+
+import android.util.Log;
+
+/**
+ * Created by xn069392 on 2017/9/21.
+ */
+
+public class C {
+    private static final String TAG = "cccc";
+
+    public static  void  gogogo(){
+        Log.e(TAG, "gogogo: ");
+    }
+}
